210322edd2604e3306755ffe697def2c1905128b
Carlator <clorenzozuniga@gmail.com>
2023-08-25 08:33:37 +0200

    - study card displaying correctly from api - card css prototyped

--- frontend/js/study.js
+++ frontend/js/study.js
@@ -3,11 +3,25 @@ const parameters = new URLSearchParams(queryString);
 const course = parameters.get('course');
 const deck = parameters.get('deck');
 
-
+var state = "question";
 
 fetch(`http://192.168.1.53:8000/api/next_card?course=${course}&deck=${deck}`, {method: "GET"})
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    
+    document.getElementById("promt-text").innerText = data.question;
+
+    document.getElementById("card").addEventListener("click", function(){
+        if (state == "question")
+        {
+            document.getElementById("promt-text").innerText = data.answer;
+
+            state = "answer";
+        } else {
+            document.getElementById("promt-text").innerText = data.question;
+
+            state = "question";
+        }
+    })
     
 })
