75290e541a115dd2d6c255e07b3ec11ac37b502d
Carlator <clorenzozuniga@gmail.com>
2023-08-27 13:28:37 +0200

    - host data config file functional

--- frontend/js/study.js
+++ frontend/js/study.js
@@ -9,71 +9,77 @@ var card = document.getElementById("card");
 var promptText = document.getElementById("prompt-text");
 
 
-
-fetch(`http://192.168.1.53:8000/api/next_card?course=${course}&deck=${deck}`, {method: "GET"})
-.then(response => response.json())
-.then(data => {
-    card.style.borderColor = confidenceColours[(data.confidence - 1)];
-
-    promptText.innerText = data.question;
-
-    updateImage(data, state);
-
-    card.addEventListener("click", function(){
-        if (state == "question")
-        {
-            promptText.innerText = data.answer;
-
-            state = "answer";
-        } else {
-            promptText.innerText = data.question;
-
-            state = "question";
-        }
-
-        updateImage(data, state);
-    })
-
-    document.addEventListener('keyup', event => {
-
-        if (event.code === 'Space') {
-            if (state == "question")
-            {
-                promptText.innerText = data.answer;
-    
-                state = "answer";
-            } else {
-                promptText.innerText = data.question;
-    
-                state = "question";
-            }
-    
-            updateImage(data, state);
-
-        } else if (event.code === "Digit1") {
-            updateConfidence(data.id, 1);
-
-        } else if (event.code === "Digit2") {
-            updateConfidence(data.id, 2);
-
-        } else if (event.code === "Digit3") {
-            updateConfidence(data.id, 3);
-
-        } 
-
+fetch("/frontend/host-info.json")
+    .then(response => response.json())
+    .then(hostData => {
+        fetch(`http://${hostData["host-ip"]}:${hostData["host-port"]}/api/next_card?course=${course}&deck=${deck}`, {method: "GET"})
+        .then(response => response.json())
+        .then(data => {
+            card.style.borderColor = confidenceColours[(data.confidence - 1)];
         
-
-      })
-
-    let confidences = document.getElementsByClassName("confidence")
-
-    for (let i = 0; i < confidences.length; i++) {
-        confidences[i].addEventListener("click", function(){
-            updateConfidence(data.id, confidences[i].id);
+            promptText.innerText = data.question;
+        
+            updateImage(data, state);
+        
+            card.addEventListener("click", function(){
+                if (state == "question")
+                {
+                    promptText.innerText = data.answer;
+        
+                    state = "answer";
+                } else {
+                    promptText.innerText = data.question;
+        
+                    state = "question";
+                }
+        
+                updateImage(data, state);
+            })
+        
+            document.addEventListener('keyup', event => {
+        
+                if (event.code === 'Space') {
+                    if (state == "question")
+                    {
+                        promptText.innerText = data.answer;
+            
+                        state = "answer";
+                    } else {
+                        promptText.innerText = data.question;
+            
+                        state = "question";
+                    }
+            
+                    updateImage(data, state);
+        
+                } else if (event.code === "Digit1") {
+                    updateConfidence(data.id, 1);
+        
+                } else if (event.code === "Digit2") {
+                    updateConfidence(data.id, 2);
+        
+                } else if (event.code === "Digit3") {
+                    updateConfidence(data.id, 3);
+        
+                } 
+        
+                
+        
+              })
+        
+            let confidences = document.getElementsByClassName("confidence")
+        
+            for (let i = 0; i < confidences.length; i++) {
+                confidences[i].addEventListener("click", function(){
+                    updateConfidence(data.id, confidences[i].id);
+                })
+            }
+            
         })
-    }
-    
+
 })
+
+
 
 function updateConfidence(id, confidence) {
     fetch(`http://192.168.1.53:8000/api/update_confidence?id=${id}&confidence=${confidence}`, {method: "GET"})
